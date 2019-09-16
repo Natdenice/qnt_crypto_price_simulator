26de7c6fef2465231437c93b1b28a2441041f8b5
Natdenice <clad1987@hotmail.com>
2019-09-16 15:18:41 +0200

    added circular icons in displayPrice component for testing purpose

--- src/components/DisplayPrice.js
+++ src/components/DisplayPrice.js
@@ -1,14 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 const displayPrice = ({ cryptoInfo }) => {
   return (
     <div>
-      <div className="ui segment">
-        <span>{cryptoInfo.priceInEth}</span>
-      </div>
-      <div className="ui segment">
-        <span>{cryptoInfo.priceInUsd}</span>
-      </div>
+      <h2 style={{ color: '#3498db' }} className="ui center aligned icon header">
+        <i style={{ color: '#3498db' }} className="circular dollar sign icon" />$
+        {cryptoInfo.priceInUsd}{' '}
+      </h2>
+      <h2 style={{ color: '#3498db' }} className="ui center aligned icon header">
+        <i style={{ color: '#3498db' }} className="circular bitcoin sign icon" />$
+        {cryptoInfo.priceInBtc}{' '}
+      </h2>
+      <h2 style={{ color: '#3498db' }} className="ui center aligned icon header">
+        <i style={{ color: '#3498db' }} className="circular ethereum sign icon" />$
+        {cryptoInfo.priceInEth}{' '}
+      </h2>
     </div>
   );
 };
